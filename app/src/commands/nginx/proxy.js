c712f6580de6c58cc4a16222950287abf3a0223c
Ciao Chung <ciao0958@gmail.com>
2021-02-05 21:48:55 +0800

    fix: 修正nginx proxy的錯誤

--- app/src/commands/nginx/proxy.js
+++ app/src/commands/nginx/proxy.js
@@ -26,8 +26,32 @@ class proxy extends Command {
       await execAsync(`sudo ln -s ${configPath} /etc/nginx/sites-enabled/${this.commandFlags.filename}`)
     }
 
+    // 有SSL的情況
+    if(this.commandFlags.ssl === true) {
+      this.email = this.commandFlags.email
+      if(!this.email) {
+        logger(`缺少email參數無法使用certbot設定SSL憑證`, 'red')
+        return
+      }
+      await execAsync(`sudo certbot --nginx --redirect --keep-until-expiring --no-eff-email --agree-tos --email ${this.email} --domains ${this.commandFlags.domain}`)
+      await this._createSslNginxConfig()
+    }
+
     await execAsync(`sudo nginx -t`)
     await execAsync(`sudo service nginx restart`)
+  }
+
+  async _createSslNginxConfig() {
+    let nginxConfigTemplate = '/nginx/site-proxy-ssl.conf'
+
+    if(!nginxConfigTemplate) {
+      logger(`找不到符合的nginx設定樣板`, 'yellow')
+      return
+    }
+    const nginxConf = global.renderService.render(nginxConfigTemplate, this.templateData)
+    const configPath = `/etc/nginx/sites-available/${this.commandFlags.filename}`
+    logger(`正在建立Nginx設定檔: ${configPath}`, 'blue')
+    await writeFileAsRoot(configPath, nginxConf)
   }
 }
 
@@ -61,6 +85,9 @@ proxy.flags = {
   host: flags.string({
     description: 'host名稱, 預設為http://127.0.0.1',
     default: 'http://127.0.0.1',
+  }),
+  email: flags.string({
+    description: 'Let\s encrypt email',
   }),
   removeSudo: global.removeSudoFlag(flags),
 }
