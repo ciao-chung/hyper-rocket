a44ccb4963d9a583cf96e0be1f89c359b26e01b1
Ciao Chung <ciao0958@gmail.com>
2022-06-22 10:49:44 +0800

    feat: nginx安裝brotli module改為選用功能

--- app/src/commands/nginx/install.js
+++ app/src/commands/nginx/install.js
@@ -23,7 +23,13 @@ class install extends Command {
       await execAsync(`sudo apt-add-repository -y ppa:hda-me/nginx-stable -r -y`)
     }
     await execAsync(`sudo apt-get update -y`)
-    await execAsync(`sudo apt-get install nginx-module-brotli -y`)
+
+    if(this.commandFlags.brotli === true) {
+      await execAsync(`sudo apt-get install nginx-module-brotli -y`, {
+        ignoreError: true,
+      })
+    }
+
     await execAsync(`sudo rm -f /etc/nginx/sites-available/default.conf`, {
       ignoreError: true,
     })
@@ -41,7 +47,10 @@ class install extends Command {
   }
 
   async _setupNginxConf() {
-    const nginxConf = global.renderService.render('nginx/nginx.conf')
+    let nginxConf = global.renderService.render('nginx/nginx.conf')
+    if(this.commandFlags.brotli === true) {
+      nginxConf = global.renderService.render('nginx/nginx-brotli.conf')
+    }
     await writeFileAsRoot('/etc/nginx/nginx.conf', nginxConf)
 
     const indexHtml = global.renderService.render('nginx/index.html')
@@ -73,6 +82,10 @@ install.description = `
 install.flags = {
   skipInstall: flags.boolean({
     description: '跳過nginx安裝',
+    default: false,
+  }),
+  brotli: flags.boolean({
+    description: '安裝nginx-module-brotli壓縮套件',
     default: false,
   }),
   certbot: flags.boolean({
